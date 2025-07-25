05e7ea63da9548e0ce211045415552b1c0c417e0
CharlesPutra <charlesagustianputra@gmail.com>
2025-07-25 07:45:21 +0700

    sejarah

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  window.addEventListener("scroll", function () {
         const navbar = document.querySelector(".navbar");
-        if (window.scrollY > 600) {
+        if (window.scrollY > 300) {
             navbar.classList.remove("navbar-transparent");
         } else {
             navbar.classList.add("navbar-transparent");
