6f6f8d8493e68a65b30ac567277715bfa1d577d2
CharlesPutra <charlesagustianputra@gmail.com>
2025-07-26 07:08:37 +0700

    memberi script konsultasi panitisa ppdb

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
- window.addEventListener("scroll", function () {
-        const navbar = document.querySelector(".navbar");
-        if (window.scrollY > 300) {
-            navbar.classList.remove("navbar-transparent");
-        } else {
-            navbar.classList.add("navbar-transparent");
-        }
-    });
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 300) {
+        navbar.classList.remove("navbar-transparent");
+    } else {
+        navbar.classList.add("navbar-transparent");
+    }
+});
+
+document.getElementById('konsulform').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const nama = document.getElementById('nama').value;
+    const nisn = document.getElementById('kontak').value;
+    const ttl = document.getElementById('pesan').value;
+
+    // Format nomor WA panitia: ganti 0 jadi 62
+    const noPanitia = "6282337631513";
+
+    const message =
+        `*Pendaftaran PPDB SMK 17 Muncar*\n\n` +
+        `📌 *Nama:* ${nama}\n` +
+        `📌 *EMAIL/NO WA:* ${kontak}\n` +
+        `📌 *PESAN:* ${pesan}\n`
+
+    const encoded = encodeURIComponent(message);
+    const waLink = `https://wa.me/${noPanitia}?text=${encoded}`;
+
+    window.open(waLink, '_blank');
+});
